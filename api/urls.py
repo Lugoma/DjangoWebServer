a9432e790a359d3465cb66a9d6d84dd199c6df83
Lugoma <luis.gomezmartinez@alum.uca.es>
2022-06-14 13:03:56 +0100

    Comments removed

--- api/urls.py
+++ api/urls.py
@@ -4,16 +4,8 @@ from .incubator.urls import urlpatterns as incubator_urls
 from .sensor.urls import urlpatterns as sensor_urls
 from .camera.urls import urlpatterns as camera_urls
 from .patient.urls import urlpatterns as patient_urls
-# se pueden crear los paths en otro modulo y hacer include() de este modulo
 
-# para empezar a hacer operaciones complejas como acceder a la lista de pacientes de un medico hace falta emplear
-# una nueva uri y crearla aqui del tipo persona/{id}/paciente
-# si a esta uri se le aniade una id al paciente entonces trae el paciente concreto, pero es absurdo, teniendo la id
-# hay que dividir las urls en diferentes ficheros (y rutas) por cada tabla, para no tenerlo todo junto
-urlpatterns = [
-    # aqui podria ir la de login por ejemplo
-    # o cosas de configuracion
-]
+urlpatterns = []
 
 urlpatterns += incubator_urls
 urlpatterns += sensor_urls
